748a30ae72f1ccecd7e9c42a49d8ef43990347b0
Fab Dubin <fabien.dubin@gmail.com>
2025-06-19 11:50:49 +0200

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="h-screen flex items-center">
+      <h1 className="text-9xl font-bold mx-4 my-8 drop-shadow-xl">
+        Welcome to the home page
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
