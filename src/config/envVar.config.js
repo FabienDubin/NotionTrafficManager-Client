616df1c7add6b9babcd9ca82f46b73e50fccb048
Fab Dubin <fabien.dubin@gmail.com>
2025-06-26 15:56:13 +0200

    fixing env var

--- src/config/envVar.config.js
+++ src/config/envVar.config.js
@@ -5,8 +5,8 @@ export function getRecaptchaKey() {
 const FALLBACK_IMG = import.meta.env.VITE_FALLBACK_IMG;
 export { FALLBACK_IMG };
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5005";
+const API_URL = import.meta.env.VITE_API_URL;
 export { API_URL };
 
-const DEFAULT_PASS = import.meta.env.VITE_DEFAULT_PASS || "Pass123";
+const DEFAULT_PASS = import.meta.env.VITE_DEFAULT_PASS;
 export { DEFAULT_PASS };
