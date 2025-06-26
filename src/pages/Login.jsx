de7893c65a95cf9d4b124e473b1141ee75dcb6c8
Fab Dubin <fabien.dubin@gmail.com>
2025-06-26 16:40:33 +0200

    rebuild with Azure App Settings env

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -71,6 +71,10 @@ const Login = () => {
       "🔎 import.meta.env.VITE_API_URL =",
       import.meta.env.VITE_API_URL
     );
+    console.log(
+      "🔎 import.meta.env.VITE_API_URL =",
+      import.meta.env.VITE_API_URL
+    );
   }, []);
 
   return (
