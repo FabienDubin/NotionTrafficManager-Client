fbe0e74301b7b35f3f53127cf10d5cf53a7e50cf
Fab Dubin <fabien.dubin@gmail.com>
2025-06-26 17:57:40 +0200

    switch to GitHub secrets for VITE_ vars

--- src/config/envVar.config.js
+++ src/config/envVar.config.js
@@ -2,11 +2,15 @@ export function getRecaptchaKey() {
   return import.meta.env.VITE_RECAPTCHA_SITE_KEY;
 }
 
-const FALLBACK_IMG = import.meta.env.VITE_FALLBACK_IMG;
+const FALLBACK_IMG =
+  import.meta.env.VITE_FALLBACK_IMG ||
+  "https://storagemercedescla01.blob.core.windows.net/profiles/ChatGPT%20Image%20May%2028,%202025,%2011_35_00%20PM.png";
 export { FALLBACK_IMG };
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5005";
+const API_URL =
+  import.meta.env.VITE_API_URL ||
+  "https://notiontrafficmanager-server-fggza5grhwdfayc0.francecentral-01.azurewebsites.net";
 export { API_URL };
 
-const DEFAULT_PASS = import.meta.env.VITE_DEFAULT_PASS;
+const DEFAULT_PASS = import.meta.env.VITE_DEFAULT_PASS || "pass123";
 export { DEFAULT_PASS };
